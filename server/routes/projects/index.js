0c467118727d4bf0941933c8ab1afc49d42d8cba
rehmpke <rehmpke@gmail.com>
2019-02-19 14:42:09 -0600

    Add projects route

--- server/routes/projects/index.js
+++ server/routes/projects/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const projectsRoute = require('./projects');
-
 module.exports = () => {
   router.get('/', (req, res, next) => {
-    return res.send('Index');
+    return res.send('All Projects');
   });
 
-  router.use('/projects', projectsRoute());
+  router.get('/:project', (req, res, next) => {
+    return res.send(`Project detail page for ${req.params.project}`);
+  });
+
   return router;
 };
