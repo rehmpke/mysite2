9f2b209f6f787d32d7a0cc0597c846e063f6110b
rehmpke <rehmpke@gmail.com>
2019-02-19 13:17:39 -0600

    Modify folder structure added server folder  and  app.js to index.js

--- server/index.js
+++ server/index.js
@@ -1,18 +1,14 @@
-'use strict';
-
 const express = require('express');
 const app = express();
 
-const PORT = 3001;
+app.listen(3001);
 
 /** ROUTES **/
 
 app.use('/static', express.static('static'));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile('index.html', {root: __dirname});
 });
 
-/** END ROUTES **/
-
-app.listen(PORT, function () { console.log('Listening on ' + PORT); });
+module.export = app;
---
+++ server/routes/index.js
@@ -0,0 +1,8 @@
+import { Module } from "module";
+
+const express = requires('express');
+const router = express.Router();
+
+module.exports = () => {
+  router.
+}
